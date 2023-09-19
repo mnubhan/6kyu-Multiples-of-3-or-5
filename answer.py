369b76f7d244313bb5a47782ea3fa2c8ff368846
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-09-19 21:46:40 +0100

    Update answer.py

--- answer.py
+++ answer.py
@@ -5,3 +5,14 @@ def solution(number):
   else:
     result=sum([x for x in range(1,number) if x%3==0 or x%5==0])
   return result
+
+def solution(number):
+    return sum(x for x in range(number) if x % 3 == 0 or x % 5 == 0)
+
+
+def solution(number):
+    sum = 0
+    for i in range(number):
+        if (i % 3) == 0 or (i % 5) == 0:
+            sum += i
+    return sum
